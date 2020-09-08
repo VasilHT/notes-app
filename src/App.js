c73fd19dc54ddad4d16f4d0ee1b88f6e355948ad
Vasil Hadzi-Tosev <vasil@stech.com>
2020-09-08 12:44:30 +0200

    Small updates to the CSS, before adding localStorage

--- src/App.js
+++ src/App.js
@@ -10,4 +10,23 @@ function App() {
   );
 }
 
+// //Save note message
+// function saveMessage() {
+//   if (typeof Storage != "undefined") {
+//     //Get value of the message
+//     var messageInput = document.getElementById("message").value;
+
+//     //Save the value in local storage.
+//     localStorage.setItem("message", messageInput);
+//     document.getElementById("message").value = localStorage.getItem("message");
+//   } else {
+//     //Throw an error if browser doesn't support local storage
+//     document
+//       .getElementById("test")
+//       .innerHTML("Sorry, your browser does not support Web Storage...");
+//   }
+// }
+// //Get the value of the message from local storage
+// document.getElementById("message").value = localStorage.getItem("message");
+
 export default App;
