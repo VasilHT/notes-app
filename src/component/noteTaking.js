971a8ee86ed1923d679cedc3830dd3445b05e898
Ertan Weezboo <ertan@stech.com>
2020-09-09 09:28:20 +0100

    commets line inside of code

--- src/component/noteTaking.js
+++ src/component/noteTaking.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class NoteTaking extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
+      //we have started to use local storage for state
       items: JSON.parse(localStorage.getItem("notes")),
     };
     this.addNote = this.addNote.bind(this);
@@ -12,40 +12,47 @@ class NoteTaking extends Component {
 
   addNote(e) {
     e.preventDefault();
+    //short naming for get local storage data
     const data = localStorage.getItem("notes");
     const newItem = {
       note: this.theNote.value,
-      //we need something unique data to manege (delete and edit) your notes, so this is basic way to use unique id
       id: Date.now(),
     };
+
+    //if local storage is undefined first we need to creat array and then we can add new item inside of it
     if (data === null) {
       let items = [];
       items.push(newItem);
       localStorage.setItem("notes", JSON.stringify(items));
+      //if local storeage is difined we can add new item inside of it
     } else {
       let items = JSON.parse(localStorage.getItem("notes"));
       items.push(newItem);
       localStorage.setItem("notes", JSON.stringify(items));
     }
 
+    //now we can update state from local storage
     this.setState({
-      items: JSON.parse(localStorage.getItem("notes"))
+      items: JSON.parse(localStorage.getItem("notes")),
     });
 
     this.theNote.value = "";
   }
 
-  //delete function added
   deleteNote = (id) => {
+    //filter deleted item from array
     let updatedItems = this.state.items.filter((item) => item.id !== id);
+    //then update state
     this.setState({
       items: updatedItems,
     });
+    //and update local storage
     localStorage.setItem("notes", JSON.stringify(updatedItems));
   };
 
   render() {
-    const myitems = this.state.items;
+    //short naming for state items
+    const items = this.state.items;
     return (
       <div>
         <header>
@@ -53,15 +60,17 @@ class NoteTaking extends Component {
         </header>
         <div className="main-content">
           <ul>
-            {myitems.length === 0
-              ? (<h2>there no notes yet</h2>)
-              : myitems.map((val) => (
-                  // you have to use key for every single item, this point is very important
-                  <li key={val.id}>
-                    {val.note}
-                    <button onClick={() => this.deleteNote(val.id)}>X</button>
-                  </li>
-                ))}
+            {/* if items array has been null or empty, shows a message */}
+            {items === null || items.length === 0 ? (
+              <h2>there is no notes yet</h2>
+            ) : (
+              items.map((val) => (
+                <li key={val.id}>
+                  {val.note}
+                  <button onClick={() => this.deleteNote(val.id)}>X</button>
+                </li>
+              ))
+            )}
           </ul>
         </div>
         <footer>
